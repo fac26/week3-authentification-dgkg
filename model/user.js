6746054d7366f1869c40415d42046d01d7539310
Georgia E. Whitney <81175151+rockyrelay@users.noreply.github.com>
2023-01-31 16:51:22 +0000

    added user js and user func

--- model/user.js
+++ model/user.js
@@ -1,1 +1,21 @@
-/* wibble wobble */
+const db = require('../database/db.js');
+
+const insert_user = db.prepare(/*sql/ `
+  INSERT INTO users (username, hash)
+  VALUES ($username, $hash)
+  RETURNING id
+`);
+
+function createUser(username, hash) {
+  return insert_user.get({ username, hash });
+}
+
+const select_user_by_username = db.prepare(/*sql*/ `
+  SELECT id, username, hash, created_at FROM users WHERE username = ?
+`);
+
+function getUserByUsername(username) {
+    return select_user_by_username.get(username);
+}
+
+module.exports = { createUser, getUserByUsername };
