2627ee0bbb5030ff9722820d958d715010dee3e4
Dominic Simpson <db_simpson@yahoo.co.uk>
2023-02-01 11:32:20 +0000

    posts js added

--- model/posts.js
+++ model/posts.js
@@ -1,13 +1,30 @@
 // require db
+const db = require('../database/db.js');
 
 // grab posts from db query
+const select_posts = db.prepare(/*sql*/ `
+  SELECT content, created_at FROM posts
+  WHERE user_id = ?
+  ΟRDER BY created_at DESC
+`);
 
-// listing posts func
+// listing posts func - using above query
+function displayPosts(user_id) {
+    return select_posts.all(user_id);
+}
 
 //  inserting posts query
 
+const insert_post = db.prepare(/*sql*/ `
+    INSERT INTO posts (content, user_id) 
+    VALUES ($content, $user_id)
+    RETURNING id, content, created_at
+`);
+
 // create posts func
 
+function createPost(content, user_id) {
+    return insert_post.get({ content, user_id });
+}
 
-
-module.exports = {}
+module.exports = { displayPosts, createPost };
