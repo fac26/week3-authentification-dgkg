c9c5ae357c649df95a8003f2e7eb5b53b76a5076
Kr33L <105356599+Kr33L@users.noreply.github.com>
2023-02-01 12:47:59 +0000

    sign-up handler

--- src/templates.js
+++ src/templates.js
@@ -1,11 +1,11 @@
-function layout({ title, content }) {
+function layout({ title, content, stylesheet }) {
     return /*html*/ `
     <!doctype html>
     <html lang="en">
       <head>
         <meta charset="UTF-8">
         <title>${title}</title>
-        <link rel="stylesheet" type="text/css" href="style.css">
+        <link rel="stylesheet" type="text/css" href="${stylesheet}">
       </head>
       <body>
         <header>
@@ -17,7 +17,7 @@ function layout({ title, content }) {
   `;
 }
 
-function form() {
+function postForm() {
     return /*html*/ `
       <form method="POST">
           <div>
@@ -26,11 +26,27 @@ function form() {
           </div>
           <div>
               <label for="comment">Enter message</label>
-              <input type="text" name="comment" id="comment" value="" />
+              <textarea type="text" name="comment" id="comment" value=""> </textarea>
           </div>
           <button type="submit">Submit</button>
       </form>
   `;
 }
 
-module.exports = { layout, form };
+function signUpForm() {
+    return /*html*/ `
+    <form method="POST">
+        <div>
+            <label for="username">Enter username</label>
+            <input type="text" name="username" id="username" value="" />
+        </div>
+        <div>
+            <label for="password">Enter password</label>
+            <input type="text" name="password" id="password" value="" />
+        </div>
+        <button type="submit">Submit</button>
+    </form>
+`;
+}
+
+module.exports = { layout, postForm, signUpForm };
