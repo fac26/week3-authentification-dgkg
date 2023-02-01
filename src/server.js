57a96787caee950c160a9b6b8845d137c87b9ab5
Georgia E. Whitney <81175151+rockyrelay@users.noreply.github.com>
2023-02-01 14:32:20 +0000

    add log-in template

--- src/server.js
+++ src/server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const cookieParser = require('cookie-parser');
 const home = require('../routes/home');
 const signUp = require('../routes/sign-up');
+const logIn = require('../routes/log-in');
 
 const server = express();
 
@@ -15,5 +16,6 @@ server.use(body);
 
 server.get('/', home.get);
 server.get('/sign-up', signUp.get);
+server.get('/log-in', logIn.get);
 
 module.exports = server;
