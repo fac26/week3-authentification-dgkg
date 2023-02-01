0bcac715cbee82e4a32997fdffa1c8125de857da
Kr33L <105356599+Kr33L@users.noreply.github.com>
2023-02-01 15:40:58 +0000

    sign-up post errors

--- routes/sign-up.js
+++ routes/sign-up.js
@@ -1,4 +1,7 @@
 const { layout, signUpForm } = require('../src/templates');
+const { createUser } = require('../model/user');
+const { createSession } = require('../model/session');
+const bcrypt = require('bcryptjs');
 
 function get(request, response) {
     const title = 'Sign up | Bikes, bikes, bikes!';
@@ -15,4 +18,26 @@ function get(request, response) {
     response.send(layout({ title, content }));
 }
 
-module.exports = { get };
+function post(request, response) {
+    const { username, password } = request.body;
+    if (!username || !password) {
+        response
+            .status(400)
+            .send('<h1>Please choose a valid new password</h1>');
+    } else {
+        bcrypt.hash(password, 12).then((hash) => {
+            const user = createUser(username, hash);
+            const session_id = createSession(user.id);
+            response.cookie('sid', session_id, {
+                signed: true,
+                httpOnly: true,
+                maxAge: 1000 * 60 * 60 * 24 * 7,
+                sameSite: 'lax',
+            });
+
+            response.redirect(`/`);
+        });
+    }
+}
+
+module.exports = { get, post };
